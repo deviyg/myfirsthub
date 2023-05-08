e95d330876cbeeb04255ea57c00ea039d2417866
deviyg <132654892+deviyg@users.noreply.github.com>
2023-05-08 17:20:42 +0530

    First commit

---
+++ First/src/firstexcel/opensheet.java
@@ -0,0 +1,30 @@
+package firstexcel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class opensheet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Devi Gandhi\\eclipse-workspace\\First\\chromedriver.exe");
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		
+		
+		options.addArguments("--disable notifications");
+		DesiredCapabilities dp= new DesiredCapabilities();
+		dp.setCapability(ChromeOptions.CAPABILITY, options);
+		options.merge(dp);
+		
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("http://172.232.75.138/HR-Management/");
+		driver.manage().window().maximize();
+		
+
+	}
+
+}
